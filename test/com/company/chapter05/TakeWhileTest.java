ddf5fa43acb1478b73de5a414da4c752b3a6319e
lkimilhol <lkimilhol@gmail.com>
2022-05-20 16:20:17 +0900

    takeWhile 학습 테스트 추가

---
+++ test/com/company/chapter05/TakeWhileTest.java
@@ -0,0 +1,25 @@
+package com.company.chapter05;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TakeWhileTest {
+
+    @Test
+    void test() {
+        // given
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // when
+        long answer = list.stream()
+                .takeWhile(i -> i < 6)
+                .count();
+
+        // then
+        assertEquals(5, answer);
+    }
+}
