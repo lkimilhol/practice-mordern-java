91cf1a83a54e52c9ca26286be981853ef290527e
lkimilhol <lkimilhol@gmail.com>
2022-05-22 11:49:32 +0900

    groupingBy 테스트 추가

--- test/com/company/chapter06/GroupTest.java
+++ test/com/company/chapter06/GroupTest.java
@@ -1,10 +1,12 @@
 package com.company.chapter06;
 
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +14,14 @@ class GroupTest {
 
     private final List<Dish> menu = List.of(
             new Dish(Type.FISH, 100, "prawns"),
-            new Dish(Type.FISH, 200, "salmon"),
-            new Dish(Type.OTHER, 300, "french fries"),
+            new Dish(Type.FISH, 450, "salmon"),
+            new Dish(Type.OTHER, 600, "french fries"),
             new Dish(Type.OTHER, 300, "rice"),
             new Dish(Type.OTHER, 100, "season fruit"),
-            new Dish(Type.OTHER, 400, "pizza"),
-            new Dish(Type.MEAT, 500, "pork"),
-            new Dish(Type.MEAT, 500, "beef"),
-            new Dish(Type.MEAT, 500, "chicken")
+            new Dish(Type.OTHER, 500, "pizza"),
+            new Dish(Type.MEAT, 800, "pork"),
+            new Dish(Type.MEAT, 800, "beef"),
+            new Dish(Type.MEAT, 800, "chicken")
     );
 
     @Test
@@ -33,4 +35,39 @@ class GroupTest {
         assertEquals(4, dishesByType.get(Type.OTHER).size());
         assertEquals(3, dishesByType.get(Type.MEAT).size());
     }
+
+    @Test
+    void groupingByCaloricLevelTest() {
+        // given
+        // when
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream().collect(
+                groupingBy(dish -> {
+                    if (dish.getCalorie() <= 400) return CaloricLevel.DIET;
+                    else if (dish.getCalorie() <= 700) return CaloricLevel.NORMAL;
+                    else return CaloricLevel.FAT;
+                }));
+
+        // then
+        assertEquals(3, dishesByCaloricLevel.get(CaloricLevel.DIET).size());
+        assertEquals(3, dishesByCaloricLevel.get(CaloricLevel.NORMAL).size());
+        assertEquals(3, dishesByCaloricLevel.get(CaloricLevel.FAT).size());
+    }
+
+    @Test
+    void maxByTest() {
+        // given
+        // when
+        Map<Type, Dish> mostCaloriesByType = menu.stream()
+                .collect(groupingBy(Dish::getType,
+                        collectingAndThen(maxBy(Comparator.comparingInt(Dish::getCalorie))
+                                , Optional::get)));
+
+        // then
+        assertEquals(450, mostCaloriesByType.get(Type.FISH).getCalorie());
+        assertEquals(600, mostCaloriesByType.get(Type.OTHER).getCalorie());
+        assertEquals(800, mostCaloriesByType.get(Type.MEAT).getCalorie());
+    }
 }
+
+enum CaloricLevel {DIET, NORMAL, FAT}
+
